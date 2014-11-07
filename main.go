bb32a60fcdfa5ad70e93a5b3c048fac7a7875a0d
unknown <abaffa@NCED2399.iis.amadeus.net>
2014-11-07 12:27:11 +0100

    Add bootstrap

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
     "net/http"
     "os"
-    "github.com/russross/blackfriday"
 )
 
 func main() {
@@ -12,12 +11,6 @@ func main() {
         port = "8080"
     }
 
-    http.HandleFunc("/markdown", GenerateMarkdown)
     http.Handle("/", http.FileServer(http.Dir("public")))
     http.ListenAndServe(":"+port, nil)
-}
-
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
-    markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
-    rw.Write(markdown)
 }
