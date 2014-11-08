d72382fadf2bf05c5559f5b9a2a6d1643034b059
Alessandro Baffa <alessandro.baffa@gmail.com>
2014-11-08 16:15:59 +0100

    Add action to handle submit button

--- hello.go
+++ hello.go
@@ -13,5 +13,11 @@ func main() {
 
     //http.Handle("/", http.FileServer(http.Dir("public")))
     http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("public"))))
+    http.HandleFunc("/assign", AssignPTR)
     http.ListenAndServe(":"+port, nil)
+}
+
+func AssignPTR(rw http.ResponseWriter, r *http.Request) {
+    markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+    rw.Write(markdown)
 }
