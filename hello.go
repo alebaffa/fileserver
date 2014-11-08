0e9e087be41a699b144f846bd8a10a78381feace
Alessandro Baffa <alessandro.baffa@gmail.com>
2014-11-08 14:08:23 +0100

    install bootstrap theme and get rid of useless route

--- hello.go
+++ hello.go
@@ -13,12 +13,6 @@ func main() {
         port = "8080"
     }
 
-    http.HandleFunc("/markdown", GenerateMarkdown)
     http.Handle("/", http.FileServer(http.Dir("public")))
     http.ListenAndServe(":"+port, nil)
-}
-
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
-    markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
-    rw.Write(markdown)
 }
